16b5b844661abb218f891bf980c4d1b0388cecc5
Carlsson Khovis <63272281+FishChipos@users.noreply.github.com>
2022-06-28 14:49:35 +0700

    Complete rewrite

--- main.py
+++ main.py
@@ -1,105 +1,54 @@
 # NOTES:
-# 1 is left and back, 0 is right and front
-# 1 is black, 0 is white
+# 1 is left, 0 is right
+# 1 is back, 0 is front
+# 1 is IR sees black or any sufficiently dark color, 
+# 0 is IR sees white or any sufficiently light color
 
 # Black magic don't touch
 pins.set_pull(DigitalPin.P20, PinPullMode.PULL_UP)
 
-# Main logic and callback functions
-def main():
-    global ir1_read, ir2_read
-    global intersectionCount, atIntersection, isMoving, isTurning, isOffCourse
-    global active
-
-    if (force_read == 0 and not active):
-        active = True
-
-    elif (force_read == 0 and active):
-        active = False
-
-    if (not active):
-        return
-
-    if (ir1_read == 0 and ir2_read == 0):
-        # atIntersection = True
-        pass
-
+# MODIFIES: NONE
 def move():
-    global isMoving, isTurning, isOffCourse, direction
+    global state
+    global direction, speed
 
-    if (not isMoving or isTurning or isOffCourse or not active):
+    # Only when moving
+    if (state == 0):
+        set_gb(direction, speed, 1 - direction, speed)
+
+# MODIFIES: adjusting, turn_direction
+def adjust():
+    global ir1_read, ir2_read
+    global state
+    global turn_direction, turn_speed
+
+    # If not adjusting then don't do anything
+    if (state != 1):
         return
 
-    set_gb(direction, speed, 1 - direction, speed)
+    # If one of the sensors sense black
+    elif (ir1_read == 0 ^ ir2_read == 0):
+        # If the left sensor doesn't sense black then turn right
+        if (ir1_read == 1):
+            turn_direction = 0
 
-    if (ir1_read == 0 and ir2_read == 1):
-        stop()
-        turn_direction = 1
-        isOffCourse = True
-        adjust_angle()
-    
-    elif (ir1_read == 1 and ir2_read == 0):
-        stop()
-        turn_direction = 0
-        isOffCourse = True
-        adjust_angle()
+        # Otherwise turn left
+        elif (ir2_read == 1):
+            turn_direction = 1
 
-def turn():
-    global isTurning, atIntersection, turn_direction
-    global ir1_read, ir2_read 
-    global turn_counter1, turn_counter2
+        set_gb(turn_direction, turn_speed, turn_direction, turn_speed)
 
-    if (not isTurning or isMoving or not active):
-        return
-
-    set_gb(turn_direction, turn_speed, turn_direction, turn_speed)
-
-    if (turn_counter1 + turn_counter2 >= 4):
-        isTurning = False
-        isMoving = True
-        atIntersection = False
-        turn_counter1 = 0
-        turn_counter2 = 0
-        return
-
-    if (ir1_read == 0 and turn_counter1 == 0):
-        turn_counter1 = 1
-
-    elif (ir1_read == 1 and turn_counter1 == 1):
-        turn_counter1 = 2
-
-    if (ir2_read == 0 and turn_counter2 == 0):
-        turn_counter2 = 1
-
-    elif (ir2_read == 1 and turn_counter2 == 1):
-        turn_counter2 = 2
-
-def read_sensors():
+# MODIFIES: ir1_read, ir2_read, force_read
+def read_pins():
     global ir1_read, ir2_read, force_read
 
     ir1_read = pins.digital_read_pin(ir1)
     ir2_read = pins.digital_read_pin(ir2)
     force_read = pins.digital_read_pin(force)
 
-def adjust_angle():
-    global turn_direction, turn_speed, isOffCourse, ir1_read, ir2_read
-
-    while isOffCourse:
-        set_gb(turn_direction, turn_speed, turn_direction, turn_speed)
-
-        if (ir1_read == 1 and ir2_read == 1):
-            isOffCourse = False
-            stop()
-            return
-
-# Utility
-def stop():
-    sensors.dd_mmotor(gb1_direction, 0, gb1_speed, 0)
-    sensors.dd_mmotor(gb2_direction, 0, gb2_speed, 0)
-
-def set_gb(gb1_dir, gb1_spd, gb2_dir, gb2_spd):
-    sensors.dd_mmotor(gb1_direction, gb1_dir, gb1_speed, gb1_spd)
-    sensors.dd_mmotor(gb2_direction, gb2_dir, gb2_speed, gb2_spd)
+def set_gb(dir1, spd1, dir2, spd2):
+    sensors.dd_mmotor(gb1_direction, dir1, gb1_speed, spd1)
+    sensors.dd_mmotor(gb2_direction, dir2, gb2_speed, spd2)
 
 # Pins
 force = DigitalPin.P20
@@ -118,7 +67,7 @@ force_read = 0
 ir1_read = 0
 ir2_read = 0
 
-# Directions for turning and moving
+# Directions for turning and moving (0 is right, 1 is left)
 direction = 0
 turn_direction = 0
 
@@ -131,21 +80,45 @@ turn_speed = 150
 turn_speed_offset1 = 0
 turn_speed_offset2 = 0
 
-active = False
+# Flags (0 -> false, 1 -> true)
+active = 0
 
-# Keep track of how many intersections the car has passed
-intersectionCount = 0
+# State (0 -> moving, 1 -> adjusting, 2 -> turning)
+state = 0
 
-# Flags for callback functions
-atIntersection = False
-isMoving = True
-isTurning = False
-isOffCourse = False
+# Array containing flags for each intersection passed
+# E.G. 
+# when passing the first intersection the first element will be set to one
+# when passing the second intersection the second element will be set to one
+# and so on
+intersections = [0, 0, 0, 0]
 
-turn_counter1 = 0
-turn_counter2 = 0
-
-basic.forever(read_sensors)
-basic.forever(main)
-basic.forever(adjust_angle)
+# Run these functions in the background
+basic.forever(read_pins)
 basic.forever(move)
+basic.forever(adjust)
+
+# Main loop
+while True:
+    # Once the button is pressed activate
+    if (force_read == 0 and active == 0):
+        active = 1
+
+    elif (force_read == 0 and active == 1):
+        # Reset
+        active = 0
+        intersections = [0, 0, 0, 0]
+        state = 0
+
+    if (active):
+        # At intersection
+        if (ir1_read == 0 and ir2_read == 0):
+            pass
+
+        # Off course
+        elif (ir1_read == 0 ^ ir2_read == 0):
+            state = 1
+
+        # Otherwise by default move forward
+        elif (ir1_read == 1 and ir2_read == 1):
+            state = 0
--- main.ts
+++ main.ts
@@ -1,64 +1,16 @@
 //  NOTES:
-//  1 is left and back, 0 is right and front
-//  1 is black, 0 is white
+//  1 is left, 0 is right
+//  1 is back, 0 is front
+//  1 is IR sees black or any sufficiently dark color, 
+//  0 is IR sees white or any sufficiently light color
 //  Black magic don't touch
 pins.setPull(DigitalPin.P20, PinPullMode.PullUp)
-//  Main logic and callback functions
-function turn() {
-    let isMoving: boolean;
-    
-    
-    
-    if (!isTurning || isMoving || !active) {
-        return
-    }
-    
-    set_gb(turn_direction, turn_speed, turn_direction, turn_speed)
-    if (turn_counter1 + turn_counter2 >= 4) {
-        isTurning = false
-        isMoving = true
-        atIntersection = false
-        turn_counter1 = 0
-        turn_counter2 = 0
-        return
-    }
-    
-    if (ir1_read == 0 && turn_counter1 == 0) {
-        turn_counter1 = 1
-    } else if (ir1_read == 1 && turn_counter1 == 1) {
-        turn_counter1 = 2
-    }
-    
-    if (ir2_read == 0 && turn_counter2 == 0) {
-        turn_counter2 = 1
-    } else if (ir2_read == 1 && turn_counter2 == 1) {
-        turn_counter2 = 2
-    }
-    
-}
-
-function adjust_angle() {
-    
-    while (isOffCourse) {
-        set_gb(turn_direction, turn_speed, turn_direction, turn_speed)
-        if (ir1_read == 1 && ir2_read == 1) {
-            isOffCourse = false
-            stop()
-            return
-        }
-        
-    }
-}
-
-//  Utility
-function stop() {
-    sensors.DDMmotor(gb1_direction, 0, gb1_speed, 0)
-    sensors.DDMmotor(gb2_direction, 0, gb2_speed, 0)
-}
-
-function set_gb(gb1_dir: number, gb1_spd: number, gb2_dir: number, gb2_spd: number) {
-    sensors.DDMmotor(gb1_direction, gb1_dir, gb1_speed, gb1_spd)
-    sensors.DDMmotor(gb2_direction, gb2_dir, gb2_speed, gb2_spd)
+//  MODIFIES: NONE
+//  MODIFIES: adjusting, turn_direction
+//  MODIFIES: ir1_read, ir2_read, force_read
+function set_gb(dir1: number, spd1: number, dir2: number, spd2: number) {
+    sensors.DDMmotor(gb1_direction, dir1, gb1_speed, spd1)
+    sensors.DDMmotor(gb2_direction, dir2, gb2_speed, spd2)
 }
 
 //  Pins
@@ -73,7 +25,7 @@ let gb2_direction = AnalogPin.P15
 let force_read = 0
 let ir1_read = 0
 let ir2_read = 0
-//  Directions for turning and moving
+//  Directions for turning and moving (0 is right, 1 is left)
 let direction = 0
 let turn_direction = 0
 //  Speeds and offsets incase one side is faster then the other (they are)
@@ -83,61 +35,77 @@ let speed_offset2 = 0
 let turn_speed = 150
 let turn_speed_offset1 = 0
 let turn_speed_offset2 = 0
-let active = false
-//  Keep track of how many intersections the car has passed
-let intersectionCount = 0
-//  Flags for callback functions
-let atIntersection = false
-let isMoving = true
-let isTurning = false
-let isOffCourse = false
-let turn_counter1 = 0
-let turn_counter2 = 0
-basic.forever(function read_sensors() {
+//  Flags (0 -> false, 1 -> true)
+let active = 0
+//  State (0 -> moving, 1 -> adjusting, 2 -> turning)
+let state = 0
+//  Array containing flags for each intersection passed
+//  E.G. 
+//  when passing the first intersection the first element will be set to one
+//  when passing the second intersection the second element will be set to one
+//  and so on
+let intersections = [0, 0, 0, 0]
+//  Run these functions in the background
+basic.forever(function read_pins() {
     
     ir1_read = pins.digitalReadPin(ir1)
     ir2_read = pins.digitalReadPin(ir2)
     force_read = pins.digitalReadPin(force)
 })
-basic.forever(function main() {
+basic.forever(function move() {
     
     
-    
-    if (force_read == 0 && !active) {
-        active = true
-    } else if (force_read == 0 && active) {
-        active = false
+    //  Only when moving
+    if (state == 0) {
+        set_gb(direction, speed, 1 - direction, speed)
     }
     
-    if (!active) {
+})
+basic.forever(function adjust() {
+    
+    
+    
+    //  If not adjusting then don't do anything
+    if (state != 1) {
         return
+    } else if (ir1_read == (0 ^ ir2_read) && (0 ^ ir2_read) == 0) {
+        //  If one of the sensors sense black
+        //  If the left sensor doesn't sense black then turn right
+        if (ir1_read == 1) {
+            turn_direction = 0
+        } else if (ir2_read == 1) {
+            //  Otherwise turn left
+            turn_direction = 1
+        }
+        
+        set_gb(turn_direction, turn_speed, turn_direction, turn_speed)
     }
     
-    if (ir1_read == 0 && ir2_read == 0) {
-        //  atIntersection = True
+})
+//  Main loop
+while (true) {
+    //  Once the button is pressed activate
+    if (force_read == 0 && active == 0) {
+        active = 1
+    } else if (force_read == 0 && active == 1) {
+        //  Reset
+        active = 0
+        intersections = [0, 0, 0, 0]
+        state = 0
+    }
+    
+    if (active) {
+        //  At intersection
+        if (ir1_read == 0 && ir2_read == 0) {
+            
+        } else if (ir1_read == (0 ^ ir2_read) && (0 ^ ir2_read) == 0) {
+            //  Off course
+            state = 1
+        } else if (ir1_read == 1 && ir2_read == 1) {
+            //  Otherwise by default move forward
+            state = 0
+        }
         
     }
     
-})
-basic.forever(adjust_angle)
-basic.forever(function move() {
-    let turn_direction: number;
-    
-    if (!isMoving || isTurning || isOffCourse || !active) {
-        return
-    }
-    
-    set_gb(direction, speed, 1 - direction, speed)
-    if (ir1_read == 0 && ir2_read == 1) {
-        stop()
-        turn_direction = 1
-        isOffCourse = true
-        adjust_angle()
-    } else if (ir1_read == 1 && ir2_read == 0) {
-        stop()
-        turn_direction = 0
-        isOffCourse = true
-        adjust_angle()
-    }
-    
-})
+}
